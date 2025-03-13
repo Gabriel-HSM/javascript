e1438ef6ad10805c534edb46bade70ec8764f42c
Gabriel Henrique <gabriel.henrique.sirgh442@gmail.com>
2025-03-12 21:09:15 -0300

    Aprendendo estrutura de repetição Do e While

---
+++ aula13/ambiente.js
@@ -0,0 +1,16 @@
+//teste lógico no fim
+var c = 1
+do {
+    console.log(`Passo ${c}`)
+    c++ // c = c + 1
+
+} while (c <= 6)
+
+/* //teste lógico no inicio
+var c = 1
+while (c <= 10){
+    console.log(`Passo ${c}`)
+    c++ // c = c + 1
+}
+
+*/
